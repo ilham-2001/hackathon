8cb6497927621c734d0970872c70a4df5a5871f9
irizqy <arizqyakbar@gmail.com>
2024-05-08 13:31:24 +0700

    add cli arg parser

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 import json
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", help = "Input file")
+
+args = parser.parse_args()
 
 load_dotenv()
+print(args.input)
 
 API_KEY = os.getenv('API_KEY')
 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('models/gemini-1.5-pro-latest')
 
-sample_file = genai.upload_file(path="kp.jpg",
+
+sample_file = genai.upload_file(path=args.input,
                             display_name="KP")
 
 print(f"Uploaded file '{sample_file.display_name}' as: {sample_file.uri}")
